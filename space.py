9885a266f35780afbcfc54b9cee29560146a3cfd
stephenpgds <106461336+stephenpgds@users.noreply.github.com>
2022-07-05 14:54:23 +0530

    Add files via upload

---
+++ space.py
@@ -0,0 +1,18 @@
+str=input()
+# c=0
+# ch=0
+# for i in str:
+#     if i==' ':
+#         if ch!=0:
+#             c=c+1
+#         ch=0
+#     else:
+#         ch=ch+1
+
+# if(ch!=0):
+#     c=c+1
+# print(c)
+# print(ch) 
+s=str.split()
+#print(s)
+print(" ".join(sorted(s)))
